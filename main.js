dd04ed6d976f4f6a391eb28e341055e7ff22992a
Andre Pimenta <trz.andre@gmail.com>
2022-01-12 17:40:24 +0000

    add more string methods

--- main.js
+++ main.js
@@ -25,3 +25,21 @@ console.log(names)
 
 names[3] = 'Gustavo'
 console.log(names)
+
+//Saber o indice/posição de algum elemento num array
+
+console.log(names.indexOf('Felipe'));
+
+//Ordenar a lista de um Array
+
+const sortedNames = names.sort();
+console.log(sortedNames);
+
+//Saber o tamanho de um Array
+
+console.log(names.length)
+
+//Validar se uma variável é um Array
+
+const namesIsArray = Array.isArray(names)
+console.log(namesIsArray)
